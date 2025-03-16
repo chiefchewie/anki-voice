688a262e5efa65ff1827a83298e8f7d1d7f7ce3c
Alan Tang <alan.tang.astro@gmail.com>
2025-03-16 00:49:00 -0400

    add some dependencies

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+    from aqt import gui_hooks
+
+    def myfunc() -> None:
+      print("myfunc")
+
+    gui_hooks.reviewer_did_show_answer.append(myfunc)
