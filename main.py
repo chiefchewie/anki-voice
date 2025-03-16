19e0584c34151efa960789cfa62f266f6bd077ab
Alan Tang <alan.tang.astro@gmail.com>
2025-03-16 01:20:44 -0400

    add __init__.py

--- main.py
+++ main.py
@@ -4,9 +4,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    from aqt import gui_hooks
-
-    def myfunc() -> None:
-      print("myfunc")
-
-    gui_hooks.reviewer_did_show_answer.append(myfunc)
