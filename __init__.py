829d92cb4fc9651dd65b8e753c3423f4caffc3d0
Alan Tang <alan.tang.astro@gmail.com>
2025-03-16 23:28:40 -0400

    playing around with hooks

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 # import the main window object (mw) from aqt
 from aqt import mw
 
+from aqt import gui_hooks
+
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo, qconnect
 
@@ -26,3 +28,17 @@ qconnect(action.triggered, testFunction)
 
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
+
+
+test_state = 1
+
+# testing with hooks
+def foo(card):
+    print(f"!!! question show with card: {card.question()}")
+
+    # this blocks ui thread, card will not be shown until foo returns
+    ease = int(input("!!! please enter something: "))
+    print(f"!!! user entered ease: {ease}")
+    mw.col.sched.answerCard(card, ease)
+
+gui_hooks.reviewer_did_show_answer.append(foo)
